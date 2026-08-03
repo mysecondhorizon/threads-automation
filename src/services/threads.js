b0d7cfbe6c83bd0af0941c7587cae0bdccbf1a1e
minyorich <minyorich@gmail.com>
2026-08-03 02:50:49 +0000

    refactor(threads): move Threads API client into services layer

---
+++ src/services/threads.js
@@ -0,0 +1,67 @@
+import { config } from "../config.js";
+
+export class ThreadsApiError extends Error {
+  constructor(step, details) {
+    super(`Threads API failed at ${step}`);
+    this.name = "ThreadsApiError";
+    this.step = step;
+    this.details = details;
+  }
+}
+
+export async function getThreadsProfile(accessToken) {
+  const url = new URL(`${config.threads.graphBase}/me`);
+
+  url.searchParams.set("fields", "id,username");
+  url.searchParams.set("access_token", accessToken);
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (!response.ok || !data.id) {
+    throw new ThreadsApiError("get_profile", data);
+  }
+
+  return data;
+}
+
+export async function publishTextPost(
+  accessToken,
+  userId,
+  text
+) {
+  const createBody = new URLSearchParams({
+    media_type: "TEXT",
+    text,
+    access_token: accessToken,
+  });
+
+  const createResponse = await fetch(
+    `${config.threads.graphBase}/${userId}/threads`,
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      body: createBody,
+    }
+  );
+
+  const createData = await createResponse.json();
+
+  if (!createResponse.ok || !createData.id) {
+    throw new ThreadsApiError(
+      "create_container",
+      createData
+    );
+  }
+
+  const publishBody = new URLSearchParams({
+    creation_id: createData.id,
+    access_token: accessToken,
+  });
+
+  const publishResponse = await fetch(
+    `${config.threads.graphBase}/${userId}/threads_publish`,
+    {
+     
