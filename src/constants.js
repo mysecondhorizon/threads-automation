1c9fb77569453c296234a5972ee908577900d1cd
minyorich <minyorich@gmail.com>
2026-08-03 01:55:30 +0000

    Add shared application constants

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const WORKER_BASE_URL =
+  "https://mysecondhorizon-threads.secondhorizon-official.workers.dev";
+
+export const OAUTH_REDIRECT_URI =
+  `${WORKER_BASE_URL}/oauth/callback`;
+
+export const ADMIN_LOGIN_URL =
+  `${WORKER_BASE_URL}/admin/login`;
+
+export const ADMIN_SESSION_TTL = 60 * 60 * 8;
+
+export const THREADS_GRAPH_BASE =
+  "https://graph.threads.net/v1.0";
